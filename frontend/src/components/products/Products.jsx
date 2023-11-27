36b6c7d912e3553744d53de171f7148df307a14d
Shashank Naik <shashanknaik808@gmail.com>
2023-11-27 11:41:17 +0530

    Created Products Component

---
+++ frontend/src/components/products/Products.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Products extends Component {
+    render() {
+        return (
+            <div>Products</div>
+        )
+    }
+}
+
+export default Products;
