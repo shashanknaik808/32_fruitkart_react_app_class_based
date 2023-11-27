c03ef9f64a717acb360a497bf720c5180ab215f3
Shashank Naik <shashanknaik808@gmail.com>
2023-11-27 16:33:35 +0530

    Updated sorting function for alphabetical ascending order.

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -31,6 +31,13 @@ export default class Products extends Component {
         }
     }
 
+    sortAlphabetAscending = () => {
+        let { productsList } = this.state.setProducts;
+        let sorted = [...productsList].sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1);
+        this.setState({ setProducts: { ...this.state.setProducts, productsList: sorted } });
+    };
+
+
     render() {
         const { flag, productsList } = this.state.setProducts;
         const { handleAddProduct } = this.props;
@@ -41,7 +48,7 @@ export default class Products extends Component {
 
                     <div className="sort-button">
                         <DropdownButton variant="success" title="Sort By ">
-                            <Dropdown.Item eventKey="1" onClick={null}>A -to- Z</Dropdown.Item>
+                            <Dropdown.Item eventKey="1" onClick={sortAlphabetAscending}>A -to- Z</Dropdown.Item>
                             <Dropdown.Item eventKey="2" onClick={null}>Z -to- A</Dropdown.Item>
                             <Dropdown.Divider />
                             <Dropdown.Item eventKey="3" onClick={null}>High to Low</Dropdown.Item>
