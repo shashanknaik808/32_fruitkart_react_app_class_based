7081ca18b1625cd2d139b46c292ead7ac258a80f
Shashank Naik <shashanknaik808@gmail.com>
2023-11-27 12:07:31 +0530

    Add navigation links and icons to Header component

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export class Header extends Component {
+export default class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <header className='header'>
+                <div>
+                    <h1>
+                        <Link to='/' className='logo'>
+                            Fruit Kart
+                        </Link>
+                    </h1>
+                </div>
+                <div className='header-links'>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                            <i class="fa-solid fa-house"></i>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <Link to="/signup">SignUp</Link>
+                            <i class="fa-solid fa-right-to-bracket"></i>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <Link to="/cart" className='cart'>
+                                <i class="fa-solid fa-cart-arrow-down"></i>
+                                <span className='cart-length'>
+
+                                </span>
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </header>
         )
     }
 }
-
-export default Header;
