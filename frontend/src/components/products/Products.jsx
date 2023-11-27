1d377f6662767c0b0282ee0f31f230252474b9ab
Shashank Naik <shashanknaik808@gmail.com>
2023-11-27 13:16:24 +0530

    Refactor product component for improved data fetching and rendering

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,11 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export class Products extends Component {
+export default class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            setProducts: {
+                flag: false,
+                productsList: null
+            }
+        }
+    }
+
+    async componentDidMount() {
+        await this.getData();
+    }
+
+    async getData() {
+        let backend_url = 'http://localhost:3300/products';
+        let response = await fetch(backend_url);
+        if (response.ok) {
+            let responseData = await response.json();
+            this.setState({
+                setProducts: { flag: true, productsList: responseData['productItems'] },
+            })
+        }
+        else {
+            console.log('error');
+        }
+    }
+
+
     render() {
+
+        const { flag, productsList } = this.state.setProducts;
         return (
-            <div>Products</div>
-        )
+            (flag) ?
+                <div className='products-container'>
+                    
+                    <div className='products'>
+                        {productsList.map(item => {
+                            return (
+                                <div className='card' key={item.name}>
+                                    <div>
+                                        <img className='product-image' src={item.image} alt={item.image} />
+                                    </div>
+                                    <div>
+                                        <h2 className='product-name'>{item.name}</h2>
+                                    </div>
+                                    <div className='product-price'>
+                                        {item.price}/-
+                                    </div>
+                                    <div>
+                                        <button className='product-add-button'
+                                            onClick={() => this.handleAddProduct(item)}>Add to Cart</button>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div> : <div><h1>Loading..!!</h1></div>
+        );
     }
 }
-
-export default Products;
