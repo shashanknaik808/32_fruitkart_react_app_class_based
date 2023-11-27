ac0702dd0010edfdfa5bbaa63a9d7211e5eb36b7
Shashank Naik <shashanknaik808@gmail.com>
2023-11-27 11:56:29 +0530

    Implement routing with BrowserRouter, Routes, and Route components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Products from './components/products/Products';
+import SignUp from './components/signup/SignUp';
+import Cart from './components/cart/Cart';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Yo , Shoot and Scoot
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header cartItems={null} />
+        <Routes>
+          <Route exact path="/" element={<Products />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
